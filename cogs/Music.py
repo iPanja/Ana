c14fcead7a8974fc90e71bd8957c65981de4ff22
Fletcher Henneman <fhenneman@comcast.net>
2022-02-05 04:01:53 -0500

    1.0.0 Music Player (Rewrite)

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,6 +1,7 @@
+from __future__ import annotations
 import discord
 from discord.ext import commands
-import pafy, urllib, re
+import pafy, urllib, re, enum, random
 from typing import List, Union
 
 def setup(bot):
@@ -9,6 +10,46 @@ def setup(bot):
         print("[Music* Module Loaded]")
     except Exception as e:
         print(" >> Music* Module: {0}".format(e))
+
+class SourceType(enum.Enum):
+    youtube = 1
+    local = 2
+
+class Source:
+    def __init__(self, address: str, source_type: SourceType, name: str):
+        self.address = address
+        self.source_type = source_type
+        self.name = name
+    def isYoutube(self) -> bool:
+        return self.source_type is SourceType.youtube
+    def isLocal(self) -> bool:
+        return self.source_type is SourceType.local
+    def get_playable_source(self, ffmpeg_settings, volume: float) -> discord.AudioSource:
+        if self.source_type == SourceType.local:
+            return discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(source=f"files/{self.address}"), volume)
+        elif self.source_type == SourceType.youtube:
+            best_quality_audio_url = pafy.new(self.address).getbestaudio().url
+            return discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(best_quality_audio_url, **ffmpeg_settings), volume)
+    @staticmethod
+    def from_youtube_link(address: str) -> Source:
+        if 'youtube.com/watch?v=' in address:
+            video_id = re.findall(r"watch\?v=(\S{11})", address)[0]
+            address = f"https://youtube.com/watch?v={video_id}"
+            return Source(address=address, source_type=SourceType.youtube, name=pafy.new(address).title)
+        return None
+    @staticmethod
+    def from_address(address: str) -> Source:
+        # Any url should be longer than 2, local files at least appended by './'
+        if len(address) < 2:
+            return None
+        if address[0:2] == './': # Local file
+            return Source(address[2::], SourceType.local, '.'.join(address.split('.')[::-1]))
+        elif 'youtube.com/watch?v=' in address: # YouTube video
+            return Source.from_youtube_link(address)
+        else:
+            return None
+    
+
     
 
 class Music(commands.Cog):
@@ -19,68 +60,52 @@ class Music(commands.Cog):
         self.volume = self.config['Music']['volume']
         self.voice_client = None
         # Queue
-        self.music_queue: List[int] = []
+        self.music_queue: List[Source] = []
+        self.is_playing = False
+        # Check if bot is currently connected to any voice channels
+        # TODO
+    
+    # /// COMMANDS
+    @commands.command(aliases=['join'])
+    async def connect(self, ctx: commands.Context):
+        self.voice_client: discord.VoiceClient = self.get_voice_client(ctx)
+        # Don't join if bot is already connected to a vc
+        if self.voice_client and self.voice_client.is_connected():
+            await ctx.send("I'm currently busy")
+            return
+        # Author must be in a vc
+        if ctx.author.voice is None:
+            await ctx.send('You must be in a voice channel!')
+            return
+        # Join vc
+        self.voice_client = await ctx.author.voice.channel.connect()
+    
+    @commands.command(aliases=['leave', 'stop'])
+    async def disconnect(self, ctx: commands.Context):
+        self.voice_client: discord.VoiceClient = self.get_voice_client(ctx)
+        # Disconnect if in a vc
+        if not self.voice_client is None:
+            await self.voice_client.disconnect()
+            self.voice_client = None
         self.is_playing = False
     
-    @commands.command(aliases=['connect'])
-    @commands.cooldown(1, 2, commands.BucketType.user)
-    async def join(self, ctx: commands.Context):
-        # If the bot is already in a voice channel
-        voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
-        if voice_client and voice_client.is_connected():
-            return
-        
-        if ctx.author.voice != None:
-            # Get voice channel that the command-issuer is in
-            author: discord.Member = ctx.author
-            vc: discord.VoiceChannel = author.voice.channel
-            # Join the channel
-            self.voice_client = await vc.connect()
-            await ctx.send('Joined {}'.format(vc))
-
-            # Play song
-            #source = await discord.FFmpegOpusAudio.from_probe("/Users/fhenneman/Documents/GitHub/Ana/BR2049_Mesa.mp3")
-            #self.voice_client.play(source)
-
-        else:
-            await ctx.send('You must be in a voice channel to use this command!')
-    
-    @commands.command(aliases=['disconnect', 'stop'])
-    @commands.cooldown(1, 2, commands.BucketType.user)
-    async def leave(self, ctx: commands.Context):
-        if self.voice_client != None:
-            await self.voice_client.disconnect()
-            await ctx.send('Left {}'.format(self.voice_client.channel))
-            self.voice_client = None
-            await ctx.message.delete()
-        
-    @commands.command()
+    @commands.command(aliases=['queue'])
     async def play(self, ctx: commands.Context):
         #https://stackoverflow.com/questions/66115216/discord-py-play-audio-from-url
-        msg = ctx.message.content
-        if 'youtube.com/watch?v=' in msg:
-            video_id = re.findall(r"watch\?v=(\S{11})", msg)[0]
-        else:
-            search = msg.replace(' ', '+')
-            html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search)
-            video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-            video_id = video_ids[0]
-        # Play/Queue Song
-        if not self.is_playing:
-            await ctx.send(f"Now playing: https://www.youtube.com/watch?v={video_id}")
-        else:
-            await ctx.send(f"Adding song to queue (Position {len(self.music_queue)+1})")
-        self.queue(video_id)
+        source = Source.from_address(address=ctx.message.content)
+        self.queue(source)
+        await self.react_with_random_emoji(ctx)
     
-    @commands.command()
-    async def play_local(self, ctx: commands.Context, filename: str):
-        self.queue(filename)
-        
+    @play.before_invoke
+    async def ensure_vc(self, ctx: commands.Context):
+        if ctx.voice_client is None: # Bot not in vc, it must connect
+            await self.connect(ctx)
     
     @commands.command()
     async def skip(self, ctx: commands.Context):
-        self.voice_client.stop()
-        self.on_song_completion(error=None)
+        if not self.voice_client is None:
+            self.voice_client.stop() # Stop current audio
+            self.on_source_completion(error=None) # Proceed normally, play next audio if applicable
     
     @commands.command()
     async def pause(self, ctx: commands.Context):
@@ -99,53 +124,29 @@ class Music(commands.Cog):
     async def volume(self, ctx: commands.Context, volume: float):
         self.volume = volume
         await ctx.send(f"Volume set to {self.volume}")
-
     
-    @play.before_invoke
-    async def ensure_vc(self, ctx):
-        #https://stackoverflow.com/questions/66115216/discord-py-play-audio-from-url
-        if ctx.voice_client is None:
-            #await self.join(ctx)
-            if ctx.author.voice:
-                self.voice_client = await ctx.author.voice.channel.connect()
-            else:
-                await ctx.send('You must be in a voice channel to use this command!')
-                raise commands.CommandError("Author not connected to a voice channel.")
-
-
-
-    # /// Helpers
-    # Get a playable audio source from a YouTube video url
-    def get_source_from_yt_url(self, url: str):
-        video = pafy.new(url)
-        best_quality_audio = video.getbestaudio()
-        # FFMPEG
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(best_quality_audio.url, **self.config['Music']['ffmpeg_settings']), self.volume)
-        return source
-    def get_source_from_local(self, filename: str):
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(source=f"files/{filename}"), self.volume)
-        return source
+    # /// HELPERS
+    async def react_with_random_emoji(self, ctx: commands.Context):
+        await ctx.message.add_reaction(random.choice(ctx.guild.emojis))
     
-    # Add YT video_id to end of queue, if queue is empty immediately play the song
-    def queue(self, video_id: Union[int, str]):
-        if len(self.music_queue) == 0 and not self.is_playing:
-            self.play_song(video_id)
-        else:
-            self.music_queue.append(video_id)      
-
-    # Physically play given song via its YouTube video id
-    def play_song(self, video_id: str):
+    def get_voice_client(self, ctx: commands.Context) -> discord.VoiceClient:
+        return discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+    
+    # Have the bot physically play audio through its mic from the given Source
+    def play_source(self, source: Source):
         self.is_playing = True
-        if video_id[0] == '.': # Local files
-            source = self.get_source_from_local(filename=video_id[1::])
-        else: # Youtube video id
-            source = self.get_source_from_yt_url("https://www.youtube.com/watch?v=" + video_id)
-        self.voice_client.play(source, after=self.on_song_completion) # On song completion, queue up the next song
-    
-    # /// Events
-    # On song completion, queue up the next song
-    def on_song_completion(self, error):
-        if len(self.music_queue) > 0:
-            self.play_song(self.music_queue.pop(0))
-        else:
+        self.voice_client.play(source.get_playable_source(self.config['Music']['ffmpeg_settings'], self.volume), after=self.on_source_completion)
+
+    # When the bot finishes playing an audio
+    def on_source_completion(self, error):
+        if len(self.music_queue) > 0: # Queue up next audio
+            self.play_source(self.music_queue.pop(0))
+        else: # Queue is empty, mark that bot is not currently playing anything
             self.is_playing = False
+
+    # Add source to queue of audios to play
+    def queue(self, source: Source):
+        if len(self.music_queue) == 0 and not self.is_playing:
+            self.play_source(source)
+        elif len(self.music_queue) + 1 <= self.config['Music']['queue_size']:
+            self.music_queue.append(source)
